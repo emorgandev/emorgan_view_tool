3eeb7182c76efc32f411f21f450d71dc8e6c0da4
Eric Morgan <ericmorgan@Erics-MacBook-Pro.local>
2018-03-24 12:49:39 -0400

    Devtool initial commit

---
+++ lib/emorgan_view_tool.rb
@@ -0,0 +1,6 @@
+require "emorgan_view_tool/version"
+require "emorgan_view_tool/renderer"
+
+module EmorganViewTool
+  # Your code goes here...
+end
---
+++ lib/emorgan_view_tool/version.rb
@@ -0,0 +1,3 @@
+module EmorganViewTool
+  VERSION = "0.1.0"
+end
